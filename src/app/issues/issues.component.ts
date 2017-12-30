246c512fc2217172eef07c7c316ab14fbc55fcf0
Fidel Diaz <fdiazro@gmail.com>
2017-12-30 18:22:16 +0100

    scroll de la tabla

--- src/app/issues/issues.component.ts
+++ src/app/issues/issues.component.ts
@@ -83,6 +83,383 @@ export class IssuesComponent implements OnInit {
         ]
       }, 
       {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
+        code: 'Issue 1',
+        description: 'Issue numero 1',
+        type: 'Retraso',
+        subtype: 'Retraso en nodo',
+        state: IssueState.Open, 
+        initialNode: {
+          mnemonic: 'ND.SID1',
+          name: 'Saiding 1',
+          shortName: 'SID1'
+        }, 
+        finalNode: {
+          mnemonic: 'ND.SID2',
+          name: 'Saiding 2',
+          shortName: 'SID2'
+        },
+        initialHour: '12:00:00',
+        finalHour:'16:00:00',
+        expectedFinalHour: '16:00:00',
+        tracks: [
+          {
+            mnemonic: 'TRK.SID1.1',
+            name: '1ST',
+            trackType: TrackType.Station,
+            initialNode: 'ND.SID1',
+            finalNode: null
+          }
+        ]
+      }, 
+      {
         code: 'Issue 2',
         description: 'Issue numero 2',
         type: 'Retraso',
