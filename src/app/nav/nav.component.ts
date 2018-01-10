c3ded15836a34d006525e98eddcc4071e0811cb3
filipmorrys <fdiazro@gmail.com>
2018-01-10 22:08:43 +0100

    navegacion

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('issuesMenu') issuesMenu: ElementRef;
+
+  @ViewChild('workOnTracksMenu') workOnTrackMenu: ElementRef;
+  
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  clickOnIssues() {
+    this.renderer.removeClass(this.workOnTrackMenu.nativeElement, 'active');
+    this.renderer.addClass(this.issuesMenu.nativeElement, 'active');
+  }
+
+  clickOnWorkOnTracks() {
+    this.renderer.addClass(this.workOnTrackMenu.nativeElement, 'active');
+    this.renderer.removeClass(this.issuesMenu.nativeElement, 'active');
   }
 
 }
